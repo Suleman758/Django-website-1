77b7ddccd27cf2dba98c005180c3dc3fb378b388
Suleman758 <f2024-0911>
2026-07-30 19:03:12 +0500

    Initial commit

---
+++ home/views.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+from django.shortcuts import render,HttpResponse
+
+from home.models import Contact
+
+# Create your views here.
+def index(request):                  
+    return render(request,'index.html')
+    #return HttpResponse("this is home page") 
+
+def about(request):
+    return HttpResponse("this is about page")
+
+def services(request):
+    return HttpResponse("this is services page")
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name,email=email,phone=phone,desc=desc,date=datetime.today())
+        contact.save()
+    return HttpResponse("this is contact page")
+
