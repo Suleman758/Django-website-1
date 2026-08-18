6976038c76db7ce34333b53f0d90b340388bfc4a
Suleman758 <f2024-0911>
2026-08-18 19:16:37 +0500

    length changed

--- home/models.py
+++ home/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
-    name = models.CharField(max_length=122)
+    name = models.CharField(max_length=105)
     email = models.CharField(max_length=122)
     phone = models.CharField(max_length=12)
     desc = models.TextField()
