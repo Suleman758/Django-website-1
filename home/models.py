5cb609564604ed141bbfeaec90edc985755aaf41
Suleman758 <f2024-0911>
2026-08-19 16:10:39 +0500

    xyz

--- home/models.py
+++ home/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
-    name = models.CharField(max_length=150)
+    name = models.CharField(max_length=145)
     email = models.CharField(max_length=100)
     phone = models.CharField(max_length=12)
     desc = models.TextField()
